eae91fceeea6ddbc1bd53ce032d9e6f4f9467971
Carlos León <mail@carlosleon.info>
2012-05-20 13:08:56 -0500

    Updating unicorn configuration.
    Signed-off-by: Carlos León <mail@carlosleon.info>

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,2 +1,11 @@
-worker_processes 4
+def workers
+  @workers ||= if ENV['WORKERS']
+    ENV['WORKERS'].to_i
+  else
+    1
+  end
+end
+
+puts "Workers: #{workers}"
+worker_processes workers
 timeout 30
